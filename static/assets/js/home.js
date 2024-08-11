fc4accb34701c71e37e6711cc24861c02c36943c
proxy-glenvar <proxy.glenvar@gmail.com>
2024-08-11 02:49:17 -0400

    home.js

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -171,47 +171,17 @@ document.addEventListener("DOMContentLoaded", event => {
     particlesJS("particles-js", particlesConfig);
   }
 });
-// Splash texts
-const SplashT = [
-  "Over 8 Million Users since 2023",
-  "Fastest growing proxy server",
-  "Made by xBubbo",
-  "Check out discord.gg/interstellar :)",
-  "Thanks for using the site",
-  "Follow us on Tiktok (@useinterstellar)",
-  "Subscribe to us on YouTube (@unblocking)",
-  "Subscribe to my Youtube (@xbubbo)",
-  "Check out the settings page",
-  "Check out our Patreon (https://www.patreon.com/gointerstellar)",
-];
 
-let SplashI = Math.floor(Math.random() * SplashT.length);
-const SplashE = document.getElementById("splash");
-
-function US() {
-  SplashI = (SplashI + 1) % SplashT.length;
-  SplashE.innerText = SplashT[SplashI];
-}
-
-SplashE.innerText = SplashT[SplashI];
-
-SplashE.addEventListener("click", US);
 // Random URL
 function getRandomUrl() {
   const randomUrls = [
-    "https://kahoot.it",
-    "https://classroom.google.com",
-    "https://drive.google.com",
-    "https://google.com",
-    "https://docs.google.com",
-    "https://slides.google.com",
-    "https://www.nasa.gov",
-    "https://blooket.com",
-    "https://clever.com",
-    "https://edpuzzle.com",
-    "https://khanacademy.org",
-    "https://wikipedia.org",
-    "https://dictionary.com",
+    "https://www.desmos.com/testing/virginia/graphing",
+    "https://va-roan-psv.edupoint.com/",
+    "https://launchpad.classlink.com/rcs/",
+    "https://launchpad.classlink.com/rcs/",
+    "https://launchpad.classlink.com/rcs/",
+    "https://launchpad.classlink.com/rcs/",
+    "https://youtube.com/",
   ];
   return randomUrls[randRange(0, randomUrls.length)];
 }
