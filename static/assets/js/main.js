bc94f3aa666355ec6442fcf6257370b00784d8b5
proxy-glenvar <proxy.glenvar@gmail.com>
2024-08-14 11:57:21 -0400

    Update main.js

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -7,16 +7,6 @@ document.addEventListener("DOMContentLoaded", () => {
     localStorage.setItem("dy", "false");
   }
 });
-
-// Nav
-const nav = document.querySelector(".fixed-nav-bar");
-
-if (nav) {
-  const themeId = localStorage.getItem("theme");
-  let LogoUrl = "/assets/media/favicon/main.png"; // Declare LogoUrl once
-  if (themeId === "Inverted") {
-    LogoUrl = "/assets/media/favicon/main-inverted.png";
-  }
   
   // Paths to the icons
   const accountIcon = "/assets/media/favicon/account.png";
@@ -27,10 +17,6 @@ if (nav) {
   const emblemicon = "/assets/media/favicon/main.png";
   
   const html = `
-    <div class="fixed-nav-bar-container">
-      <div id="icon-container">
-        <a class="icon" href="/./"><img alt="nav" id="INImg" src="${LogoUrl}"/></a>
-      </div>
     </div>
     <div class="fixed-nav-bar-right">
       <a class="navbar-link" href="/./"><img src="${emblemicon}" alt="emblem" class="navbar-img" /></a>
